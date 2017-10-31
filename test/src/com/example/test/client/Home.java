68377529f1046ec7f0fc18d49e79e8c991ce44d6
shanu776 <gokusupersayin9@gmail.com>
2017-10-31 15:06:19 +0530

    MenuBar

--- test/src/com/example/test/client/Home.java
+++ test/src/com/example/test/client/Home.java
@@ -1,28 +1,37 @@
 package com.example.test.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.datepicker.client.DatePicker;
 
 public class Home implements EntryPoint {
-	Button btn;
 	
+	DatePicker dp = new DatePicker();
 	@Override
 	public void onModuleLoad() {
-		btn = new Button("click");
-		RootPanel.get().add(btn);
-		
-		btn.addClickHandler(new ClickHandler() {
+		Command cmd = new Command() {
 			
 			@Override
-			public void onClick(ClickEvent event) {
+			public void execute() {
 				// TODO Auto-generated method stub
-				Window.alert("button clicked!");
+				Window.alert("working");
 			}
-		});
+		};
+		MenuBar menu = new MenuBar();
+		MenuBar menuFile = new MenuBar(true);
+		menuFile.addItem("open",cmd);
+		menuFile.addItem("save",cmd);
+		menuFile.addItem("saveAs",cmd);
+		menuFile.addItem("close",cmd);
+		menu.addItem("File",menuFile);
+		menu.addItem("Tool",cmd);
+		menu.addItem("Option",cmd);
+		
+		RootPanel.get().add(menu);
+		
 	} 
 
 }
