da36779b1949d1b135554f595452e7d9f6abe241
shanu776 <gokusupersayin9@gmail.com>
2017-10-31 13:22:01 +0530

    UIBinder

---
+++ test/src/com/example/test/client/Home.java
@@ -0,0 +1,15 @@
+package com.example.test.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class Home implements EntryPoint {
+
+	LittelForm form  = new LittelForm();
+	@Override
+	public void onModuleLoad() {
+		RootPanel.get().add(form);
+	}
+
+}
